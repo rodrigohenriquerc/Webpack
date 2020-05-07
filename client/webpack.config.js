c23eebaca5d244babb2873dca6b767c770c76eef
Rodrigo Henrique <rhrc09@gmail.com>
2020-05-07 19:40:56 -0300

    Installed Babili and configured it in Webpack to uglify the babel content when building the production version; installed Cross Env to make the Node environment variable work in other platforms, not only MacOS.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,11 @@
 const path = require('path');
+const babiliPlugin = require('babili-webpack-plugin');
+
+let plugins = [];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(new babiliPlugin());
+}
 
 module.exports = {
   // First module to be loaded.
@@ -18,5 +25,6 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins
 }
